b29e937e3214b430d797d4024f7ef81f16695599
Mahtab Tadayon <32145218+mimifi@users.noreply.github.com>
2020-04-13 17:16:11 +0200

    Break main.dart file (#8)

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Save Recipies',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.white
-      ),
-      home: HomeScreen(),
-    );
+    return
   }
 }
